a45b23095a86515fb48b409c63ad26f76c52c2f9
Yell0wAt0m <jjh92625@gmail.com>
2022-10-29 18:24:23 +0100

    Disabled morph mod plugin if workshop addon is missing

--- plugins/pillcmd/plugin/sh_plugin.lua
+++ plugins/pillcmd/plugin/sh_plugin.lua
@@ -1,5 +1,9 @@
 
 local PLUGIN = PLUGIN;
-local Clockwork = Clockwork;
+
+if not pk_pills then
+	MsgC(Color(255, 50, 50), "[Morph-Mod Enforcer] Pill Pack not found, not loading!\n")
+	return
+end
 
 Clockwork.kernel:IncludePrefixed("sv_hooks.lua");
