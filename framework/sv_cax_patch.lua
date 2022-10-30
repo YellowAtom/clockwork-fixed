221f94bd14556e5cb0f53516d51315d984d157bf
Yell0wAt0m <jjh92625@gmail.com>
2022-10-30 14:01:43 +0000

    Cleaned up Cax Patch

--- framework/sv_cax_patch.lua
+++ framework/sv_cax_patch.lua
@@ -1,53 +1,41 @@
-print("Please wait while Clockwork initializes...")
-
-CloudAuthX = CWUtil;
-CloudAuthX.kernel = {};
+CloudAuthX = CWUtil
+CloudAuthX.kernel = {}
 
 function CloudAuthX.GetVersion()
-	return CLOUDAUTHX_VERSION;
-end;
+	return CLOUDAUTHX_VERSION
+end
 
 function CloudAuthX.kernel:IncludeSchema()
-	local schemaFolder = Clockwork.kernel:GetSchemaFolder();
+	local schemaFolder = Clockwork.kernel:GetSchemaFolder()
+	if schemaFolder == "" then return end
 
-	if (schemaFolder == "") then
-		return;
-	end;
-
-	Clockwork.config:Load(nil, true);
-	Clockwork.plugin:Include(schemaFolder .. "/schema", true);
-	Clockwork.config:Load();
-end;
+	Clockwork.config:Load(nil, true)
+	Clockwork.plugin:Include(schemaFolder .. "/schema", true)
+	Clockwork.config:Load()
+end
 
 function CloudAuthX.Base64Encode(data)
-	if (!data) then
-		return "";
-	end;
+	if not data then return "" end
 
-	return util.Base64Encode(data);
-end;
+	return util.Base64Encode(data)
+end
 
 function CloudAuthX.Base64Decode(data)
-	if (!data) then
-		return "";
-	end;
+	if not data then return "" end
 
-	return util.Base64Decode(data);
-end;
+	return util.Base64Decode(data)
+end
 
 function CloudAuthX.External(data)
-	MsgC(Color(255, 100, 0, 255), "[Clockwork] CloudAuthX.External is no longer supported.\n");
+	MsgC(Color(255, 100, 0, 255), "[Clockwork] CloudAuthX.External is no longer supported.\n")
 	debug.Trace()
-end;
+end
 
-local cwOldRunConsoleCommand = RunConsoleCommand;
+local cwOldRunConsoleCommand = RunConsoleCommand
 
 function RunConsoleCommand(...)
-	local arguments = {...};
+	local arguments = {...}
 
-	if (arguments[1] == nil) then
-		return;
-	end;
-
-	cwOldRunConsoleCommand(...);
-end;
+	if arguments[1] == nil then return end
+	cwOldRunConsoleCommand(...)
+end
