cfe9b8b5a27b729abc55c8758b860c3eb3afce4c
Yell0wAt0m <jjh92625@gmail.com>
2022-11-22 22:47:55 +0000

    Better fix for CSC

--- plugins/crossserverchat/plugin/sv_hooks.lua
+++ plugins/crossserverchat/plugin/sv_hooks.lua
@@ -1,49 +1,52 @@
 
-if Clockwork.database:IsSQLite() then
-	MsgC(Color(255, 255, 0), "[CrossServerChat] This plugin will not load with SQLite!\n")
-	return
-end
-
 -- Called when the Clockwork database has connected.
 function cwXCS:ClockworkDatabaseConnected()
-	local CREATE_MESSAGES_TABLE = [[
-	CREATE TABLE IF NOT EXISTS `]] .. Clockwork.config:Get("mysql_xsc_table"):Get() .. [[` (
-		`_Key` int(11) NOT NULL AUTO_INCREMENT,
-		`_ServerName` varchar(255) NOT NULL,
-		`_PlayerName` varchar(255) NOT NULL,
-		`_Text` text NOT NULL,
-		`_Color` varchar(255) NOT NULL,
-		PRIMARY KEY (`_Key`)
-	) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;]]
-	Clockwork.database:Query(string.gsub(CREATE_MESSAGES_TABLE, "%s", " "), nil, nil, true)
+	if not Clockwork.database:IsSQLite() then
+		local CREATE_MESSAGES_TABLE = [[
+			CREATE TABLE IF NOT EXISTS `]] .. Clockwork.config:Get("mysql_xsc_table"):Get() .. [[` (
+				`_Key` int(11) NOT NULL AUTO_INCREMENT,
+				`_ServerName` varchar(255) NOT NULL,
+				`_PlayerName` varchar(255) NOT NULL,
+				`_Text` text NOT NULL,
+				`_Color` varchar(255) NOT NULL,
+				PRIMARY KEY (`_Key`)
+			) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;]]
+		Clockwork.database:Query(string.gsub(CREATE_MESSAGES_TABLE, "%s", " "), nil, nil, true)
+	else
+		MsgC(Color(255, 255, 0), "[CrossServerChat] This plugin will not load with SQLite!\n")
+	end
 end
 
 -- Called when a player attempts to say something out-of-character.
 function cwXCS:PlayerCanSayOOC(player, text)
-	local curTime = CurTime()
-	local playerName = player:Name()
-	local serverName = Clockwork.config:Get("cross_server_chat_name"):Get()
-	local teamColor = Clockwork.json:Encode(cwTeam.GetColor(player:Team()))
+	if not Clockwork.database:IsSQLite() then
+		local curTime = CurTime()
+		local playerName = player:Name()
+		local serverName = Clockwork.config:Get("cross_server_chat_name"):Get()
+		local teamColor = Clockwork.json:Encode(cwTeam.GetColor(player:Team()))
 
-	if Clockwork.config:Get("cross_server_chat_enabled"):Get() and playerName ~= "" and serverName ~= "" and text ~= "" then
-		if not player.nextTalkOOC or curTime > player.nextTalkOOC then
-			local queryObj = Clockwork.database:Insert("messages")
-			queryObj:SetValue("_ServerName", serverName)
-			queryObj:SetValue("_PlayerName", playerName)
-			queryObj:SetValue("_Text", text)
-			queryObj:SetValue("_Color", teamColor)
-			queryObj:Push()
+		if Clockwork.config:Get("cross_server_chat_enabled"):Get() and playerName ~= "" and serverName ~= "" and text ~= "" then
+			if not player.nextTalkOOC or curTime > player.nextTalkOOC then
+				local queryObj = Clockwork.database:Insert("messages")
+				queryObj:SetValue("_ServerName", serverName)
+				queryObj:SetValue("_PlayerName", playerName)
+				queryObj:SetValue("_Text", text)
+				queryObj:SetValue("_Color", teamColor)
+				queryObj:Push()
+			end
 		end
 	end
 end
 
 -- Called each tick.
 function cwXCS:Tick()
-	local curTime = CurTime()
+	if not Clockwork.database:IsSQLite() then
+		local curTime = CurTime()
 
-	if Clockwork.config:Get("cross_server_chat_enabled"):Get() then
-		if not self.cwNextQueryChat or curTime >= self.cwNextQueryChat then
-			self:QueryChat()
+		if Clockwork.config:Get("cross_server_chat_enabled"):Get() then
+			if not self.cwNextQueryChat or curTime >= self.cwNextQueryChat then
+				self:QueryChat()
+			end
 		end
 	end
 end
