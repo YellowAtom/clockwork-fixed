7d0290b723641c1481f0342366b2556daf7c39b0
Atronode <55639134+Atronode@users.noreply.github.com>
2024-12-30 03:00:15 +0000

    Revert "runechat"
    This reverts commit c48ef1861628c9879f851493b982061542979edf.

--- plugins/runechat/plugin/sh_plugin.lua
+++
@@ -1,7 +0,0 @@
-/* -------------------------------------------------------------------------- */
-/*                                  RUNECHAT                                  */
-/* -------------------------------------------------------------------------- */
-PLUGIN:SetGlobalAlias("runechat");
-
-Clockwork.kernel:IncludePrefixed("sv_plugin.lua");
-Clockwork.kernel:IncludePrefixed("cl_plugin.lua");
