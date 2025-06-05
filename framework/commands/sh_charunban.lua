c8fdd329e5159b68c66336215ba987929fdf7c5a
Atronode <55639134+Atronode@users.noreply.github.com>
2025-06-06 00:04:51 +0100

    Update sh_charunban.lua

--- framework/commands/sh_charunban.lua
+++ framework/commands/sh_charunban.lua
@@ -3,7 +3,7 @@ local COMMAND = Clockwork.command:New("CharUnban")
 COMMAND.tip = "CmdCharUnban"
 COMMAND.text = "CmdCharUnbanDesc"
 COMMAND.flags = CMD_DEFAULT
-COMMAND.access = "a"
+COMMAND.access = "o"
 COMMAND.arguments = 1
 
 -- Called when the command has been run.
